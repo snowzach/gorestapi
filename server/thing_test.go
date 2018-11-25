5990dac384c641d2a1a6525d416d84b24b57f5e3
Zach Brown <zach.brown@coinninja.com>
2018-11-24 23:00:24 -0500

    Updated additional test cases

--- server/thing_test.go
+++ server/thing_test.go
@@ -15,13 +15,12 @@ import (
 
 func TestServerThingPost(t *testing.T) {
 
-	// Mock Store
+	// Mock Store and server
 	ts := new(mocks.ThingStore)
-	// Create Server
 	s, err := New(ts)
 	assert.Nil(t, err)
 
-	// Create Mock Item
+	// Create Item
 	i := &gorestapi.Thing{
 		ID:   "id",
 		Name: "name",
@@ -40,6 +39,93 @@ func TestServerThingPost(t *testing.T) {
 	// Make request and validate we get back proper response
 	e := httpexpect.New(t, server.URL)
 	e.POST("/things").WithJSON(i).Expect().Status(http.StatusOK).JSON().Object().Equal(&idResponse)
+
+	// Check remaining expectations
+	ts.AssertExpectations(t)
+
+}
+
+func TestServerThingGetAll(t *testing.T) {
+
+	// Mock Store and server
+	ts := new(mocks.ThingStore)
+	s, err := New(ts)
+	assert.Nil(t, err)
+
+	// Create Item
+	i := []*gorestapi.Thing{
+		&gorestapi.Thing{
+			ID:   "id1",
+			Name: "name1",
+		},
+		&gorestapi.Thing{
+			ID:   "id2",
+			Name: "name2",
+		},
+	}
+
+	// Mock call to item store
+	ts.On("ThingFind", mock.AnythingOfType("*context.valueCtx")).Once().Return(i, nil)
+
+	// Create test server
+	server := httptest.NewServer(s.router)
+	defer server.Close()
+
+	// Make request and validate we get back proper response
+	e := httpexpect.New(t, server.URL)
+	e.GET("/things").Expect().Status(http.StatusOK).JSON().Array().Equal(&i)
+
+	// Check remaining expectations
+	ts.AssertExpectations(t)
+
+}
+
+func TestServerThingGet(t *testing.T) {
+
+	// Mock Store and server
+	ts := new(mocks.ThingStore)
+	s, err := New(ts)
+	assert.Nil(t, err)
+
+	// Create Item
+	i := &gorestapi.Thing{
+		ID:   "id",
+		Name: "name",
+	}
+
+	// Mock call to item store
+	ts.On("ThingGetByID", mock.AnythingOfType("*context.valueCtx"), "1234").Once().Return(i, nil)
+
+	// Create test server
+	server := httptest.NewServer(s.router)
+	defer server.Close()
+
+	// Make request and validate we get back proper response
+	e := httpexpect.New(t, server.URL)
+	e.GET("/things/1234").Expect().Status(http.StatusOK).JSON().Object().Equal(&i)
+
+	// Check remaining expectations
+	ts.AssertExpectations(t)
+
+}
+
+func TestServerThingDelete(t *testing.T) {
+
+	// Mock Store and server
+	ts := new(mocks.ThingStore)
+	s, err := New(ts)
+	assert.Nil(t, err)
+
+	// Mock call to item store
+	ts.On("ThingDeleteByID", mock.AnythingOfType("*context.valueCtx"), "1234").Once().Return(nil)
+
+	// Create test server
+	server := httptest.NewServer(s.router)
+	defer server.Close()
+
+	// Make request and validate we get back proper response
+	e := httpexpect.New(t, server.URL)
+	e.DELETE("/things/1234").Expect().Status(http.StatusNoContent)
 
 	// Check remaining expectations
 	ts.AssertExpectations(t)
